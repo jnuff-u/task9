90fe55c7eafe3ca570aa2cf59ee75b6872e44578
jnuff-u <137416943@qq.com>
2018-07-07 02:10:21 +0800

    task9

---
+++ tuscanyClien/src/main/java/com/opt/util/safe/COSClientCredentials.java
@@ -0,0 +1,16 @@
+package com.opt.util.safe;
+
+public class COSClientCredentials {
+
+
+    public static final String BUCKET = "ap-chengdu";
+
+    public static final String BUCKET_NAME = "opz-1251253153";
+
+    public static final String SECRET_ID = "AKIDHMBk36bPjqlSvAkyXt1wd6MPEpAGoi37";
+
+    public static final String SECRET_KEY = "uYw3vWquhEWYJo2zg7HBoBH6fz3uXw86";
+
+    public static final String FOLDER = "stat/images/";
+
+}
